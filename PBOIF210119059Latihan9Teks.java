485d819ba6440c217fbc2bc8e1ffa4d77ba591f2
irgidp <irgijak@gmail.com>
2020-10-15 22:17:55 +0700

    Latihan 9

---
+++ PBOIF210119059Latihan9Teks.java
@@ -0,0 +1,24 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan teks
+ */
+package pboif2.pkg10119059.latihan9.teks;
+
+public class PBOIF210119059Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char :" + tab + "berisi " + c);
+    }
+    
+}
